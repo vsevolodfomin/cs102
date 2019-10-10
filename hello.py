16b4d9c690bf2ea9bf2fecedcaba6a9a71370615
vsevolodfomin <56412098+vsevolodfomin@users.noreply.github.com>
2019-10-10 23:10:10 +0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -4,3 +4,8 @@ git add README.md
 git commit -m "first commit"
 git remote add origin https://github.com/vsevolodfomin/cs102.git
 git push -u origin master
+def message():
+    print("Hello, World!")
+
+if __name__ == "__main__":
+    message()
