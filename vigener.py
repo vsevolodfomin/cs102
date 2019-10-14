fe1132a45d4d65eedd097deefc1c2b5f857b68a4
vsevolodfomin <56412098+vsevolodfomin@users.noreply.github.com>
2019-10-14 11:51:01 +0300

    Update vigener.py

--- vigener.py
+++ vigener.py
@@ -1,19 +1,26 @@
-def encrypt_vigenere(plaintext, keyword):
+def encrypt_vigenere(plaintext, key):
+    key_length = len(key)
+    key_as_int = [ord(i) for i in key]
+    plaintext_int = [ord(i) for i in plaintext]
     ciphertext = ''
-    for index, ch in enumerate(plaintext):
-        if 'a' <= ch <= 'z' or 'A' <= ch <= 'Z':
-            shift = ord(keyword[index % len(keyword)])
-            shift -= ord('a') if 'a' <= ch <= 'z' else ord('A')
-            code = ord(ch) + shift
-            if 'a' <= ch <= 'z' and code > ord('z'):
-                code -= 26
-            elif 'A' <= ch <= 'Z' and code > ord('Z'):
-                code -= 26
-            ciphertext += chr(code)
-        else:
-            chiphertext += ch
+    for i in range(len(plaintext_int)):
+        value = (plaintext_int[i] + key_as_int[i % key_length]) % 26
+        ciphertext += chr(value + 65)
     return ciphertext
+
+def decrypt_vigenere(ciphertext, key):
+    key_length = len(key)
+    key_as_int = [ord(i) for i in key]
+    ciphertext_int = [ord(i) for i in ciphertext]
+    plaintext = ''
+    for i in range(len(ciphertext_int)):
+        value = (ciphertext_int[i] - key_as_int[i % key_length]) % 26
+        plaintext += chr(value + 65)
+    return plaintext
 
 p1=input("Enter you word: ")
 k1=input("Enter you keyword: ")
-print("Result:", encrypt_vigenere(p1,k1))
+
+print("Result:", encrypt_vigenere(p1, k1))
+n1 = encrypt_vigenere(p1, k1)
+print("Result:", decrypt_vigenere(n1, k1))
