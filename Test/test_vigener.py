8b8f18f86d200e8eda9ccfe8baad8ddb91ecc31f
vsevolodfomin <vsevolodfomin@gmail.gom>
2019-10-15 18:31:30 +0300

    Update test_vigener.py

--- Test/test_vigener.py
+++ Test/test_vigener.py
@@ -1,41 +1,46 @@
+import unittest
+import doctest
+import caesar
+"""
+
 def encrypt_vigenere(plaintext, key):
-    key_length = len(key)
-    key_as_int = [ord(i) for i in key]
-    plaintext_int = [ord(i) for i in plaintext]
-    ciphertext = ''
-    for i in range(len(plaintext_int)):
-        if 65 <= plaintext_int[i] <= 90:
-            value = (plaintext_int[i] + key_as_int[i % key_length]) % 26
-            ciphertext += chr(value + 65)
-        elif 97 <= plaintext_int[i] <= 122:
-            # 12 - change between alphabets 
-            value = (plaintext_int[i] + key_as_int[i % key_length] - 12) % 26
-            ciphertext += chr(value + 97)
-        else:
-            ciphertext += chr(plaintext_int[i])
-    return ciphertext
 
 
 def decrypt_vigenere(ciphertext, key):
-    key_length = len(key)
-    key_as_int = [ord(i) for i in key]
-    ciphertext_int = [ord(i) for i in ciphertext]
-    plaintext = ''
-    for i in range(len(ciphertext_int)):
-        if 65 <= ciphertext_int[i] <= 90:
-            value = (ciphertext_int[i] - key_as_int[i % key_length]) % 26
-            plaintext += chr(value + 65)
-        elif 97 <= ciphertext_int[i] <= 122:
-            value = (ciphertext_int[i] - key_as_int[i % key_length]) % 26
-            plaintext += chr(value + 97)
-        else:
-            plaintext += chr(ciphertext_int[i])
-    return plaintext
+
+class CaesarCipherTest(unittest.TestCase):
+
+    def test_encrypt_empty_string(self):
+        self.assertEqual(caesar.encrypt_caesar(""), "")
+
+    def test_encrypt_uppercase(self):
+        self.assertEqual(caesar.encrypt_caesar("PYTHON"), "SBWKRQ")
+
+    def test_encrypt_lowercase(self):
+        self.assertEqual(caesar.encrypt_caesar("python"), "sbwkrq")
+
+    def test_encrypt_uppercase_and_lowercase(self):
+        self.assertEqual(caesar.encrypt_caesar("Python"), "Sbwkrq")
+
+    def test_encrypt_uppercase_lowercase_and_digits(self):
+        self.assertEqual(caesar.encrypt_caesar("Python3.6"), "Sbwkrq3.6")
+
+    def test_decrypt_empty_string(self):
+        self.assertEqual(caesar.decrypt_caesar(""), "")
+
+    def test_decrypt_uppercase(self):
+        self.assertEqual(caesar.decrypt_caesar("SBWKRQ"), "PYTHON")
+
+    def test_decrypt_lowercase(self):
+        self.assertEqual(caesar.decrypt_caesar("sbwkrq"), "python")
+
+    def test_decrypt_uppercase_and_lowercase(self):
+        self.assertEqual(caesar.decrypt_caesar("Sbwkrq"), "Python")
+
+    def test_decrypt_uppercase_lowercase_and_digits(self):
+        self.assertEqual(caesar.decrypt_caesar("Sbwkrq3.6"), "Python3.6")
 
 """
-p1 = input("Enter you word: ")
-k1 = input("Enter you keyword: ")
-n1 = encrypt_vigenere(p1, k1)
-print("Result encript:", n1)
-print("Result decripr", decrypt_vigenere(n1, k1))
-"""
+
+
+
